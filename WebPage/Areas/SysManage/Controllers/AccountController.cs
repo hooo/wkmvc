b9dec7a617670a1a38744ade08b5cc3f2d636de7
hooo <wgk3721@gmail.com>
2016-07-20 13:48:16 +0800

    登录功能的实现，完善登录功能

--- WebPage/Areas/SysManage/Controllers/AccountController.cs
+++ WebPage/Areas/SysManage/Controllers/AccountController.cs
@@ -37,27 +37,53 @@ namespace WebPage.Areas.SysManage.Controllers
             var json = new JsonHelper() { Msg = "登录成功", Status = "n" };
             try
             {
-                //调用登录验证接口 返回用户实体类
-                var users = UserManage.UserLogin(item.ACCOUNT.Trim(), item.PASSWORD.Trim());
-                if (users != null)
+                //获取表单验证码
+                var code = Request.Form["code"];
+                if(Session["gif"] != null)
                 {
-                    //是否锁定
-                    if (users.ISCANLOGIN == 1)
+                    //判断验证码是否正确
+                    if(!string.IsNullOrEmpty(code) && code.ToLower() == Session["gif"].ToString().ToLower())
                     {
-                        json.Msg = "用户已锁定，禁止登录，请联系管理员进行解锁";
-                        log.Warn(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
-                        return Json(json);
-                    }
-                    json.Status = "y";
-                    log.Info(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
+                        //调用登录验证接口 返回用户实体类
+                        var users = UserManage.UserLogin(item.ACCOUNT.Trim(), item.PASSWORD.Trim());
+                        if (users != null)
+                        {
+                            //是否锁定
+                            if (users.ISCANLOGIN == 1)
+                            {
+                                json.Msg = "用户已锁定，禁止登录，请联系管理员进行解锁";
+                                log.Warn(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
+                                return Json(json);
+                            }
 
+                            var account = this.UserManage.GetAccountByUser(users);
+                            //记录用户信息到Cookies
+                            string cookievalue = "{\"id\":\"" + account.Id + "\",\"username\":\"" + account.LogName +
+                                                                            "\",\"password\":\"" + account.PassWord + "\",\"ToKen\":\"" +
+                                                                            Session.SessionID + "\"}";
+                            CookieHelper.SetCookie("cookie_rememberme", new Common.CryptHelper.AESCrypt().Encrypt(cookievalue), null);
+
+                            //更新用户本次登录IP
+                            users.LastLoginIP = Utils.GetIP();
+                            UserManage.Update(users);
+
+                            json.Status = "y";
+                            json.ReUrl = "/Sys/Home/Index";
+                            log.Info(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
+
+                        }
+                        else
+                        {
+                            json.Msg = "用户名或密码不正确";
+                            log.Error(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
+                        }
+                    }
                 }
                 else
                 {
-                    json.Msg = "用户名或密码不正确";
+                    json.Msg = "验证码已过期，请刷新验证码";
                     log.Error(Utils.GetIP(), item.ACCOUNT, Request.Url.ToString(), "Login", "系统登录，登录结果：" + json.Msg);
                 }
-
             }
             catch (Exception e)
             {
@@ -68,7 +94,20 @@ namespace WebPage.Areas.SysManage.Controllers
         }
         #endregion
 
+
         #region 帮助方法
+        /// <summary>
+        /// 验证码 
+        /// </summary>
+
+        public FileContentResult ValidateCode()
+        {
+            string code = "";
+            System.IO.MemoryStream ms = new Models.verify_code().Create(out code);
+            Session["gif"] = code;//验证码存储在Session中,供验证
+            Response.ClearContent();//清空输出流
+            return File(ms.ToArray(), @"image/png");
+        }
         #endregion
     }
 }
