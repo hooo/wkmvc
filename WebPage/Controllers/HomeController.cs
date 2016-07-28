dd45e5afa3ec02fe9f62d2d702bbd23879fbc2f4
hooo <wgk3721@gmail.com>
2016-07-28 21:46:10 +0800

    主目录加入HomeControl跳转到SysManage/Account/Index

---
+++ WebPage/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Account", new { Area = "SysManage" });
+        }
+    }
+}
